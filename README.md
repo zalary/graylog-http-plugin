f4b4022f2822c30cc00eaf64e792eadb72815ec3
Revanna Sagar <sagar.revanna@kudelskisecurity.com>
2018-09-10 13:47:55 +0530

    Updated README.md for more informative installation guide.
    Graylog plugin to forward stream data to HTTP.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# graylog-http-plugin
-Graylog plugin to post Stream data to HTTP.
+# HttpOutput Plugin for Graylog
+
+**Required Graylog version:** 2.0 and later
+
+This Plugin has a very specific purpose:  Write the messages to an Http, indicated in the Plugin configuration parameters.
+
+Getting started
+---------------
+
+This project is using Maven 3 and requires Java 8 or higher.
+
+Installation
+------------
+[Download the plugin](https://github.com/sagarinpursue/graylog-http-plugin)
+
+* Clone this repository.
+* Run `mvn package` to build a JAR file.
+* Optional: Run `mvn jdeb:jdeb` and `mvn rpm:rpm` to create a DEB and RPM package respectively.
+* Copy generated JAR file in target directory to your Graylog plugin directory.
+* Restart the Graylog.
+
+The plugin directory is the `plugins/` folder relative from your `graylog-server` directory by default
+and can be configured in your `graylog.conf` file.
+
+Restart `graylog-server` and you are done.
+
+Usage
+-----
+
+Once you have installed the plugin, you can configure an Output of type  com.plugin.HttpOutput, with this simple parameter:
+
+output_api: API where the stream message is forwarded to.
+
+Plugin Release
+--------------
+
+We are using the maven release plugin:
+
+```
+$ mvn release:prepare
+[...]
+$ mvn release:perform
+```
---
+++ src/main/java/com/plugin/HttpOutput.java
@@ -0,0 +1,147 @@
+package com.plugin;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.graylog2.plugin.Message;
+import org.graylog2.plugin.configuration.Configuration;
+import org.graylog2.plugin.configuration.ConfigurationRequest;
+import org.graylog2.plugin.configuration.fields.ConfigurationField;
+import org.graylog2.plugin.configuration.fields.TextField;
+import org.graylog2.plugin.outputs.MessageOutput;
+import org.graylog2.plugin.streams.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * This is the plugin. Your class should implement one of the existing plugin
+ * interfaces. (i.e. AlarmCallback, MessageInput, MessageOutput)
+ */
+public class HttpOutput implements MessageOutput {
+
+	private boolean shutdown;
+	private String url;
+	private static final String CK_OUTPUT_API = "output_api";
+	private static final Logger LOG = LoggerFactory.getLogger(HttpOutput.class);
+
+	@Inject
+	public HttpOutput(@Assisted Stream stream, @Assisted Configuration conf) throws HttpOutputException {
+
+		url = conf.getString(CK_OUTPUT_API);
+		shutdown = false;
+		LOG.info(" Http Output Plugin has been configured with the following parameters:");
+		LOG.info(CK_OUTPUT_API + " : " + url);
+		
+		try {
+            final URL urlTest = new URL(url);
+        } catch (MalformedURLException e) {
+        	LOG.info("Error in the given API", e);
+            throw new HttpOutputException("Error while constructing the API.", e);
+        }
+	}
+
+	@Override
+	public boolean isRunning() {
+		return !shutdown;
+	}
+
+	@Override
+	public void stop() {
+		shutdown = true;
+
+	}
+
+	@Override
+	public void write(List<Message> msgs) throws Exception {
+		for (Message msg : msgs) {
+			writeBuffer(msg.getFields());
+		}
+	}
+
+	@Override
+	public void write(Message msg) throws Exception {
+		if (shutdown) {
+			return;
+		}
+
+		writeBuffer(msg.getFields());
+	}
+
+	public void writeBuffer(Map<String, Object> data) throws HttpOutputException {
+		OkHttpClient client = new OkHttpClient();
+		Gson gson = new Gson();
+
+		try {
+			final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+			RequestBody body = RequestBody.create(JSON, gson.toJson(data));
+			Request request = new Request.Builder().url(url).post(body).build();
+			Response response = client.newCall(request).execute();
+			if (response.code() != 200) {
+				LOG.info("Unexpected HTTP response status " + response.code());
+				throw new HttpOutputException("Unexpected HTTP response status " + response.code());
+			}
+			response.close();
+
+		} catch (IOException e) {
+			LOG.info("Error while posting the stream data to the given API", e);
+            throw new HttpOutputException("Error while posting stream to HTTP.", e);
+		}
+
+	}
+
+	public interface Factory extends MessageOutput.Factory<HttpOutput> {
+		@Override
+		HttpOutput create(Stream stream, Configuration configuration);
+
+		@Override
+		Config getConfig();
+
+		@Override
+		Descriptor getDescriptor();
+	}
+
+	public static class Descriptor extends MessageOutput.Descriptor {
+		public Descriptor() {
+			super("HttpOutput Output", false, "", "Forwards stream to HTTP.");
+		}
+	}
+
+	public static class Config extends MessageOutput.Config {
+		@Override
+		public ConfigurationRequest getRequestedConfiguration() {
+			final ConfigurationRequest configurationRequest = new ConfigurationRequest();
+			configurationRequest.addField(new TextField(CK_OUTPUT_API, "API to forward the stream data.", "/",
+					"HTTP address where the stream data to be sent.", ConfigurationField.Optional.NOT_OPTIONAL));
+
+			return configurationRequest;
+		}
+	}
+
+	public class HttpOutputException extends Exception {
+
+		private static final long serialVersionUID = -5301266791901423492L;
+
+		public HttpOutputException(String msg) {
+            super(msg);
+        }
+
+        public HttpOutputException(String msg, Throwable cause) {
+            super(msg, cause);
+        }
+
+    }
+}
